bbcc8108f5eea3d6c5fbf5a0e79b8e81fde8811e
ladyalice <alicejoyk@gmail.com>
2013-08-06 23:02:37 -0700

    Add Busted Paperclip

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,7 +1,15 @@
 class Pic < ActiveRecord::Base
-  attr_accessible :description
-
-  belongs_to :user
-  validates :user_id, presence: true
+  
+  
+  has_attached_file :image, styles: { medium: "320x240>"}
+attr_accessible :description, :image
+validates :user_id, presence: true
   validates :description, presence: true
+  belongs_to :user
+  validates :attachment, :attachment_content_type => { :content_type => ['image/png', 'image/jpg', 'image/gif']}, :presence => true
+  
+  
+  
+  
 end
+
